f8c89cd5814d5365a149e42419911e1065abe6d8
Ahmed Hassan <ahmed__hassan@outlook.com>
2023-08-17 14:53:30 +0300

    refactor: New SVG to JSX transformer package ♻️

---
+++ packages/parcel-transformer-svg-jsx/src/index.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) GeeekyBoy
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { Transformer } from "@parcel/plugin";
+import { transform } from "@svgr/core";
+import svgoPlugin from "@svgr/plugin-svgo";
+import jsxPlugin from "@svgr/plugin-jsx";
+
+export default new Transformer({
+  async transform({ asset }) {
+    const code = await asset.getCode();
+    const compiled = await transform(
+      code,
+      {
+        runtimeConfig: false,
+        jsxRuntime: "automatic",
+        expandProps: false,
+        template: (variables, { tpl }) => tpl`
+        function ${variables.componentName} (${variables.props}) {
+          return (${variables.jsx});
+        }
+        ${variables.exports};
+      `,
+      },
+      {
+        caller: {
+          name: "@mango-js/parcel-transformer-svg-jsx",
+          defaultPlugins: [svgoPlugin, jsxPlugin],
+        },
+        filePath: asset.filePath,
+      },
+    );
+    asset.type = "js";
+    asset.bundleBehavior = null;
+    asset.setCode(compiled);
+    return [asset];
+  },
+});
