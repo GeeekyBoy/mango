72a53c9c14e00f86a2c12a0a2c4c7c5849643ea7
Ahmed Hassan <ahmed__hassan@outlook.com>
2024-04-22 01:17:30 +0200

    docs: Update code of remote function demo 📝

--- website/src/util/encryption.remote.js
+++ website/src/util/encryption.remote.js
@@ -1,12 +1,12 @@
 // A function that encrypts a string using RSA encryption
 import crypto from "crypto";
 
-const algorithm = "aes256";
+const algorithm = "aes-256-cbc";
+const iv = crypto.randomBytes(16);
 const secretKey = "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3";
 
-
 export async function encrypt(msg) {
-  const cipher = crypto.createCipher(algorithm, secretKey);
+  const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey), iv);
   let encryptedMsg = cipher.update(msg, "utf8", "hex");
   encryptedMsg += cipher.final("hex");
   return encryptedMsg;
@@ -14,7 +14,7 @@ export async function encrypt(msg) {
 
 export async function decrypt(encryptedMsg) {
   try  {
-    const decipher = crypto.createDecipher(algorithm, secretKey);
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey), iv);
     let decryptedMsg = decipher.update(encryptedMsg, "hex", "utf8");
     decryptedMsg += decipher.final("utf8");
     return decryptedMsg;
